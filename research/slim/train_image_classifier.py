59bd72cb13eda4612485277e8a866aa4ddd36084
xiaxia <kathyxxj@service-inf-drzs-vpn-091.ethz.ch>
2020-10-10 14:45:33 +0200

    update default parameters

--- research/slim/train_image_classifier.py
+++ research/slim/train_image_classifier.py
@@ -203,10 +203,10 @@ tf.app.flags.DEFINE_string(
     'as `None`, then the model_name flag is used.')
 
 tf.app.flags.DEFINE_integer(
-    'batch_size', 32, 'The number of samples in each batch.')
+    'batch_size', 8, 'The number of samples in each batch.')
 
 tf.app.flags.DEFINE_integer(
-    'train_image_size', None, 'Train image size')
+    'train_image_size', 224, 'Train image size')
 
 tf.app.flags.DEFINE_integer('max_number_of_steps', None,
                             'The maximum number of training steps.')
